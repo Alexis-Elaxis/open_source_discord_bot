f671f39ffef7a8979aff2bfb2d569ef82a422f8d
Elaxis-Dev <52383750+Elaxis-Dev@users.noreply.github.com>
2020-07-14 10:47:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const client = new Discord.client();
-const config = require("./config.json");
+const config = require("./includes/config.json");
+
+client.on('ready', guild => {
+  client.user.setActivity(config.prefix + `help for help`);
+});
+
+client.login(config.token);
