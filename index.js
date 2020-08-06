37e03c3dddd32fb38a3a162a8e296212a9274efb
Elaxis-Dev <52383750+Elaxis-Dev@users.noreply.github.com>
2020-08-06 23:23:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require("./includes/config.json");
 
 client.on('ready', guild => {
   client.user.setActivity(config.prefix + `help for help`);
+  console.log(`I'm ready bro !`);
 });
 
 client.login(config.token);
