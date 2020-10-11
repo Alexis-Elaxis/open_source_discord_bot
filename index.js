0745cdcb5b8e01e4f68660ae4f9ca65bd378e2ba
Alexis-Elaxis <52383750+Alexis-Elaxis@users.noreply.github.com>
2020-10-11 11:02:15 +0200

    Update index.js
    Adding the test command

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const client = new Discord.client();
 const config = require("./includes/config.json");
 
 client.on('ready', guild => {
-  client.user.setActivity(config.prefix + `help for help`);
-  console.log(`I'm ready bro !`);
+  client.user.setActivity(config.prefix + `test for test`);
+  console.log(`I'm ready !`);
 });
+
+client.on("message" () => {
+  if(message.channel.type === "dm") return;
+  if(message.guild || message.author.bot) return;
+  
+  let args = message.content.split(" ");
+  let command = args.shift().toLowerCase();
+  let message.content = message.content.substr(config.prefix.length);
+  
+  if(command == "test") {
+   message.channel.send("It's okey !").catch(err => {
+    console.log(err)
+   }); 
+  }
+})
 
 client.login(config.token);
